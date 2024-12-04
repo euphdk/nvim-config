f5085f0fe6dd42a52dc8d7be96870aab7f640a47
Dennis Skærup Højlund Andersen <dennan@norlys.dk>
2024-12-04 12:10:23 +0100

    ...

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -42,3 +42,4 @@ vim.opt.iskeyword:append '-' -- hyphenated words recognized by searches
 vim.opt.formatoptions:remove { 'c', 'r', 'o' } -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
 vim.opt.runtimepath:remove '/usr/share/vim/vimfiles' -- separate vim plugins from neovim in case vim still in use
 vim.o.spell = false
+vim.g.go_recommended_style = 0
